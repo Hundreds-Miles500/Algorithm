8c9e2465a88636d04c1907f90dc3705b71484e8e
Aaron <zhangjiayi2002@outlook.com>
2022-09-09 00:47:17 +0800

    add import StdStats

--- Algorithm/percolation/PercolationStats.java
+++ Algorithm/percolation/PercolationStats.java
@@ -5,6 +5,7 @@
  **************************************************************************** */
 
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
